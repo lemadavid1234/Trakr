f242d200db45aaf3d4b25cf426f02f28223a83e6
David Lema <lemadavid1234@gmail.com>
2025-06-18 12:47:21 -0400

    Set up Firebase initialization with auth and Firestore

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+//Initialize Firebase - gives access to all Firebase services
+const app = initializeApp(firebaseConfig);
+
+//Export Firebase Analytics Service - tracks user behavior and app usage 
+export const analytics = getAnalytics(app);
+
+//Export Firebase Authentication Service - handles user sign-up, login, logout, etc
+export const auth = getAuth(app);
+
+//Export Firestore database service - read/write user data like workout logs or plans
+export const db = getFirestore(app);
